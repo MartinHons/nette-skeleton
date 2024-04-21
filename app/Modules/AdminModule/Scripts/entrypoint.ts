5699171dd8d192715c0f287485dedbffcd8e7fde
Martin Hons <hons46@gmail.com>
2024-04-21 16:40:36 +0200

    zakladni nasazeni sablony tabler

--- app/Modules/AdminModule/Scripts/entrypoint.ts
+++ app/Modules/AdminModule/Scripts/entrypoint.ts
@@ -1,11 +1,6 @@
-import 'bootstrap'
+import './tabler/tabler'
 import naja from 'naja';
-
 naja.initialize();
 
-import './layout/topbar'
-import './layout/spinner'
-import './layout/sidebar'
-import './layout/toast'
 
 
--- app/Modules/AdminModule/Scripts/layout/toast.ts
+++
@@ -1,7 +0,0 @@
-const container = document.getElementById('toast-container')
-
-if (container) {
-    container.querySelectorAll('.toast').forEach(toast => {
-        toast.classList.add('show')
-    })
-}
